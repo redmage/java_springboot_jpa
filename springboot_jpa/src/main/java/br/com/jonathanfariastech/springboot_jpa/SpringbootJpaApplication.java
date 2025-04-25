00c327b6a211c6ea787329be453aafa631acef0c
redmage <akronus.oluigi@gmail.com>
2025-04-25 16:28:03 +0000

    first commit

---
+++ springboot_jpa/src/main/java/br/com/jonathanfariastech/springboot_jpa/SpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.jonathanfariastech.springboot_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaApplication.class, args);
+	}
+
+}
